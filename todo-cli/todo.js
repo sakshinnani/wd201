6fde3457837394677b976a6581fb06e7146772f7
sakshinnani <141358546+sakshinnani@users.noreply.github.com>
2023-08-25 01:35:38 +0530

    todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -72,30 +72,4 @@ const todoList = () => {
       toDisplayableList,
     };
   };
-  
-  // ####################################### #
-  // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
-  // ####################################### #
-  
-  const todos = todoList();
-  
-  const formattedDate = (d) => {
-    return d.toISOString().split("T")[0];
-  }
-  
-  var dateToday = new Date();
-  const today = formattedDate(dateToday);
-  const yesterday = formattedDate(
-    new Date(new Date().setDate(dateToday.getDate() - 1))
-  );
-  const tomorrow = formattedDate(
-    new Date(new Date().setDate(dateToday.getDate() + 1))
-  );
-  
-  todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false });
-  todos.add({ title: 'Pay rent', dueDate: today, completed: true });
-  todos.add({ title: 'Service Vehicle', dueDate: today, completed: false });
-  todos.add({ title: 'File taxes', dueDate: tomorrow, completed: false });
-  todos.add({ title: 'Pay electric bill', dueDate: tomorrow, completed: false });
-  
-  console.log("My Todo-list\n")
+module.exports = todolist;
