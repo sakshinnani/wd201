eacee03cee849d9847105abd53eae26370cf23ed
sakshi innani <sakshi.innani.cse@ghrce.raisoni.net>
2023-09-07 14:21:22 +0530

    Hi

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -11,12 +11,89 @@ module.exports = (sequelize, DataTypes) => {
     static async addTask(params) {
       return await Todo.create(params);
     }
-    static associate(models) {
-      // define association here
+    static async showList() {
+      console.log("My Todo list \n");
+
+      console.log("Overdue");
+      // FILL IN HERE
+      console.log("My Todo list \n");
+
+      console.log("Overdue");
+      console.log(
+        (await Todo.overdue())
+          .map((todo) => todo.displayableString())
+          .join("\n"),
+      );
+
+      console.log("\n");
+
+      console.log("Due Today");
+      // FILL IN HERE
+      console.log(
+        (await Todo.dueToday())
+          .map((todo) => todo.displayableString())
+          .join("\n"),
+      );
+      console.log("\n");
+
+      console.log("Due Later");
+      console.log(
+        (await Todo.dueLater())
+          .map((todo) => todo.displayableString())
+          .join("\n"),
+      );
     }
+
+    static async overdue() {
+      // FILL IN HERE TO RETURN OVERDUE ITEMS
+      return await Todo.findAll({
+        where: {
+          dueDate: { [Op.lt]: new Date().toLocaleDateString("en-CA") },
+        },
+      });
+    }
+
+    static async dueToday() {
+      // FILL IN HERE TO RETURN ITEMS DUE tODAY
+      return await Todo.findAll({
+        where: {
+          dueDate: { [Op.eq]: new Date().toLocaleDateString("en-CA") },
+        },
+      });
+    }
+
+    static async dueLater() {
+      // FILL IN HERE TO RETURN ITEMS DUE LATER
+      return await Todo.findAll({
+        where: {
+          dueDate: { [Op.gt]: new Date().toLocaleDateString("en-CA") },
+        },
+      });
+    }
+
+    static async markAsComplete(id) {
+      // FILL IN HERE TO MARK AN ITEM AS COMPLETE
+
+      await Todo.update(
+        { completed: true },
+        {
+          where: {
+            id: id,
+          },
+        },
+      );
+    }
+
     displayableString() {
       let checkbox = this.completed ? "[x]" : "[ ]";
-      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
+      const now = new Date();
+      const formatted_Date = now.toISOString().substring(0, 10);
+      let dueDate = this.dueDate;
+      if (dueDate !== formatted_Date) {
+        return `${this.id}. ${checkbox} ${this.title} ${dueDate}`.trim();
+      } else {
+        return `${this.id}. ${checkbox} ${this.title}`.trim();
+      }
     }
   }
   Todo.init(
--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,47 +1,46 @@
 /* eslint-disable no-undef */
 const todoList = () => {
-    let all = [];
-    const add = (todoItem) => {
-      all.push(todoItem);
-    };
-    const markAsComplete = (index) => {
-      all[index].completed = true;
-    };
-  
-    const overdue = () => {
-        let todaysdate = new Date().toISOString().split('T')[0];
-        return all.filter((itemlist) => itemlist.dueDate < todaysdate );
-
-    };
-  
-    const dueToday = () => {
-        let todaysdate = new Date().toISOString().split('T')[0];
-        return all.filter((itemlist) => itemlist.dueDate === todaysdate);
-    };
-  
-    const dueLater = () => {
-      let todaysdate = new Date().toISOString().split('T')[0];
-      return all.filter((item) => item.dueDate > todaysdate);
-    };
-    const toDisplayableList = (list) => {
-      return list
-        .map(
-          (todo) =>
-            `${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${
-              todo.dueDate == todaysdate ? "" : todo.dueDate
-            }`
-        )
-        .join("\n");
-    };
-    return { 
-        all,
-        add, 
-        markAsComplete, 
-        overdue, 
-        dueToday,
-         dueLater,
-        toDisplayableList, 
-    };
+  let all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
   };
-  
-  module.exports = todoList;
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    let todaysdate = new Date().toISOString().split("T")[0];
+    return all.filter((itemlist) => itemlist.dueDate < todaysdate);
+  };
+
+  const dueToday = () => {
+    let todaysdate = new Date().toISOString().split("T")[0];
+    return all.filter((itemlist) => itemlist.dueDate === todaysdate);
+  };
+
+  const dueLater = () => {
+    let todaysdate = new Date().toISOString().split("T")[0];
+    return all.filter((item) => item.dueDate > todaysdate);
+  };
+  const toDisplayableList = (list) => {
+    return list
+      .map(
+        (todo) =>
+          `${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${
+            todo.dueDate == todaysdate ? "" : todo.dueDate
+          }`,
+      )
+      .join("\n");
+  };
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+
+module.exports = todoList;
