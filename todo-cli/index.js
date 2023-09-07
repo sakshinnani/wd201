84747d8199825a4ed49eeb8a6129ddb0840d1981
sakshi innani <sakshi.innani.cse@ghrce.raisoni.net>
2023-09-07 14:05:17 +0530

    Hi

---
+++ todo-cli/index.js
@@ -0,0 +1,96 @@
+// index.js
+const { connect } = require("./connectDB.js");
+const Todo = require("./TodoModel.js");
+const updateItem = async (id) => {
+  try {
+    const todo = await Todo.update(
+      { completed: true },
+      {
+        where: {
+          id: id,
+        },
+      },
+    );
+
+    console.log(todo.displayableString());
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+(async () => {
+  // await createTodo();
+  // await countItems();
+  await getAllTodos();
+  await updateItem(2);
+  await getAllTodos();
+})();
+
+const getSingleTodo = async () => {
+  try {
+    const todo = await Todo.findOne({
+      where: {
+        completed: false,
+      },
+      order: [["id", "DESC"]],
+    });
+
+    console.log(todo.displayableString());
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+(async () => {
+  // await createTodo();
+  // await countItems();
+  await getSingleTodo();
+})();
+
+const getAllTodos = async () => {
+  try {
+    const todos = await Todo.findAll();
+    const todoList = todos.map((todo) => todo.displayableString()).join("\n");
+    console.log(todoList);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+(async () => {
+  // await createTodo();
+  // await countItems();
+  await getAllTodos();
+})();
+// index.js
+const countItems = async () => {
+  try {
+    const totalCount = await Todo.count();
+    console.log(`Found ${totalCount} items in the table!`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+(async () => {
+  // await createTodo();
+  await countItems();
+})();
+
+// index.js
+const createTodo = async () => {
+  try {
+    await connect();
+    const todo = await Todo.addTask({
+      title: "Second Item",
+      dueDate: new Date(),
+      completed: false,
+    });
+    console.log(`Created todo with ID : ${todo.id}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+(async () => {
+  await createTodo();
+})();
